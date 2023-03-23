d275072aa79b67c151f7021821a693d8e384cb38
Ng Geok Theng <gthengng@gmail.com>
2023-03-23 19:05:37 +0800

    Modify script.js

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function addBookToLibrary(){
     let title = document.getElementById('title').value;
     let author = document.getElementById('author').value;
     let pages = document.getElementById('pages').value;
-    let read = document.querySelector('input[name="read_status"]:checked').value;
-    
+    let read = document.getElementById('read').checked;
+
     let newBook = new Book(title, author, pages, read);
 
     myLibrary.push(newBook);
@@ -21,41 +21,46 @@ function addBookToLibrary(){
 }
 
 function adding() {
-    let bookLibrary = document.getElementById('book-library');
-    bookLibrary.innerHTML = "";
+    const library = document.getElementById('library');
+    library.innerHTML = '';
 
     for (let i = 0; i < myLibrary.length; i++) {
         let book = myLibrary[i];
         let bookCard = document.createElement('div');
-        bookCard.className = 'bookCard';
-        bookCard.style.borderColor = 'orange';
-        bookCard.style.backgroundColor = 'orange';
-        bookCard.style.width = '35vh';
-        bookCard.style.margin = '0.5rem';
-        bookCard.style.padding = '0.3rem';
-        bookCard.style.textAlign = 'center';
-        bookCard.innerHTML = `
-            <div class="book">
-                <h3>${book.title}</h2>
-                <h4>by ${book.author}</h3>
-                <p>Pages: ${book.pages}</p>
-                <p>Read status: ${book.read}</p>
-            </div>    
-        `;
-        bookLibrary.append(bookCard);
+        bookCard.className = 'book-card';
+
+        let bookTitle = document.createElement('div');
+        bookTitle.className = 'book-title';
+        bookTitle.innerHTML = book.title;
+
+        let bookAuthor = document.createElement('div');
+        bookAuthor.className = 'book-author';
+        bookAuthor.innerHTML = book.author;
+
+        let bookPages = document.createElement('div');
+        bookPages.className = 'book-pages';
+        bookPages.innerHTML = `Pages: ${book.pages}`;
+
+        let removeBtn = document.createElement('button');
+        removeBtn.className = 'remove-btn';
+        removeBtn.innerHTML = 'REMOVE';
+        
+        bookCard.append(bookTitle, bookAuthor, bookPages, removeBtn);
+
+        library.append(bookCard);
     }
 }
 
-let newBookBtn = document.querySelector('#new-book-btn');
-let newBookForm = document.querySelector('#new-book-form');
+const newBookBtn = document.getElementById('new-book-btn')
+const newBookForm = document.getElementById('new-book-form');
 
-newBookBtn.addEventListener('click', function(){
+newBookBtn.addEventListener('click', function() {
     newBookForm.style.display = 'block';
 })
 
-newBookForm.addEventListener('submit', function(e){
+newBookForm.addEventListener('submit', function(e) {
     e.preventDefault();
     addBookToLibrary();
-    newBookForm.style.display = 'none';
     newBookForm.reset();
+    newBookForm.style.display = 'none';
 })
